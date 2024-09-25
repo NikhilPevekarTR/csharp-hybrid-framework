b2f9c6c57f74a984709ba337f27fdc592a378a89
Nikhil Pevekar <nikhil.pevekar@thomsonreuters.com>
2024-09-25 11:39:40 +0530

    Added TestCaseSource and separated test method and test data

---
+++ MedicalRecordAutomation/LoginTest.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThomsonReuters.MedicalRecordAutomation.Base;
+using ThomsonReuters.MedicalRecordAutomation.Utilities;
+
+namespace ThomsonReuters.MedicalRecordAutomation
+{
+    public class LoginTest : AutomationWrapper
+    {
+        [Test]
+        [TestCaseSource(typeof(DataSource), nameof(DataSource.ValidLoginData))]
+        public void ValidLoginTest(string username, string password, string expectedValue)
+        {
+            driver.FindElement(By.Id("authUser")).SendKeys(username);
+            driver.FindElement(By.CssSelector("#clearPass")).SendKeys(password);
+            driver.FindElement(By.Id("login-button")).Click();
+
+            //Assert the title  - OpenEMR
+            Assert.That(driver.Title, Is.EqualTo(expectedValue));
+        }
+
+        [Test]
+        [TestCase("saul", "saul234", "Invalid username or password")]
+        public void invalidLoginTest(string username, string password, string expectedError)
+        {
+            driver.FindElement(By.Id("authUser")).SendKeys(username);
+            driver.FindElement(By.CssSelector("#clearPass")).SendKeys(password);
+            driver.FindElement(By.Id("login-button")).Click();
+
+            //Assert the error - Invalid username or password
+            string actualError = driver.FindElement(By.XPath("//p[contains(text(),'Invalid')]")).Text;
+
+            Assert.That(actualError.Contains(expectedError)); //expect true
+        }
+    }
+}
--- MedicalRecordAutomation/LoginUITest.cs
+++ MedicalRecordAutomation/LoginUITest.cs
@@ -1,25 +1,22 @@
-﻿using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThomsonReuters.MedicalRecordAutomation.Base;
 
 namespace ThomsonReuters.MedicalRecordAutomation
 {
-    //Login Test Cases
-    public class LoginUITest
+    /// <summary>
+    /// Login UI Test Cases
+    /// </summary>
+    public class LoginUITest : AutomationWrapper
     {
         [Test]
         public void TitleTest()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-
-            driver.Navigate().GoToUrl("https://demo.openemr.io/b/openemr");
-
             string actualTitle = driver.Title;
             Assert.That(actualTitle, Is.EqualTo("OpenEMR Login"));
         }
@@ -27,12 +24,6 @@ namespace ThomsonReuters.MedicalRecordAutomation
         [Test]
         public void ApplicationDescriptionTest()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-
-            driver.Navigate().GoToUrl("https://demo.openemr.io/b/openemr");
-
             //Assert the text --> "The most popular open-source Electronic Health Record and Medical Practice Management solution."
             string actualDescription = driver.FindElement(By.XPath("//p[contains(text(),'most')]")).Text;
             Assert.That(actualDescription, Is.EqualTo("The most popular open-source Electronic Health Record and Medical Practice Management solution."));
